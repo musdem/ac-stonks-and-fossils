1b672c68b59a5e828b28995c9e30c0bb71b6c8f5
Zack Munich <zmunich@architech.ca>
2020-04-06 11:59:33 -0400

    added basic colors and structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ModalComponent } from './shared/modal/modal.component';
-import { ColumnContainerComponent } from './shared/column-container/column-container.component';
 import { ServicesComponent } from './services/services.component';
 import { StonkDashboardComponent } from './dashboards/stonk-dashboard/stonk-dashboard.component';
 import { FossilsDashboardComponent } from './dashboards/fossils-dashboard/fossils-dashboard.component';
@@ -13,7 +12,6 @@ import { FossilsDashboardComponent } from './dashboards/fossils-dashboard/fossil
   declarations: [
     AppComponent,
     ModalComponent,
-    ColumnContainerComponent,
     ServicesComponent,
     StonkDashboardComponent,
     FossilsDashboardComponent
--- src/app/dashboards/stonk-dashboard/stonk-dashboard.component.ts
+++ src/app/dashboards/stonk-dashboard/stonk-dashboard.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StonkDashboardComponent implements OnInit {
 
+  user = {
+    name: 'Zack',
+    price: 140,
+    priceBought: 91,
+    turnipsBought: 1960
+  };
+  investors = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    // TODO pull in investor data from API
+    this.investors = [
+      {
+        name: 'Annie',
+        price: 150
+      },
+      {
+        name: 'Connor',
+        price: 160
+      },
+      {
+        name: 'Ohan',
+        price: 170
+      },
+      {
+        name: 'Matthew',
+        price: 80
+      }
+    ];
   }
 
 }
